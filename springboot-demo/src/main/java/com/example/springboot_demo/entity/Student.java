7a0734bb9664f15fdaf01efe95e500cc07f20494
Dastagiri <121944040+Dastagiri-s@users.noreply.github.com>
2025-07-20 00:09:50 +0530

    Update Student.java
    added constructor using fields

--- springboot-demo/src/main/java/com/example/springboot_demo/entity/Student.java
+++ springboot-demo/src/main/java/com/example/springboot_demo/entity/Student.java
@@ -6,6 +6,14 @@ public class Student {
     private  String name;
     private String email;
 
+
+    public Student( Integer id,String name,String email)
+    {
+        this.id=id;
+        this.name=name;
+        this.email=email;
+    }
+
     public Integer getId() {
         return id;
     }
