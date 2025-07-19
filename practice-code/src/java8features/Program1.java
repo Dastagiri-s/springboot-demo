b4baf25b763d9dd3248de2428b62043cbfb45aa5
Dastagiri <121944040+Dastagiri-s@users.noreply.github.com>
2025-07-19 18:36:18 +0530

    Added first practice code

---
+++ practice-code/src/java8features/Program1.java
@@ -0,0 +1,10 @@
+package java8features;
+
+public class Program1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("This is a Java 8 feature demonstration.");
+    }
+}
